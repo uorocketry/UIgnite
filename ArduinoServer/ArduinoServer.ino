93cbe9f908c21c56501d4dab286aeabf4b8d78be
Paul Buzuloiu <pbuzu025@uottawa.ca>
2018-10-09 12:19:09 -0400

    aggregate client and server

---
+++ ArduinoServer/ArduinoServer.ino
@@ -0,0 +1,162 @@
+#include <Servo.h>
+#include <Arduino.h>
+
+Servo servo;
+
+int close_servo = 17;
+int servoPin = 9;
+
+const int relay = 7; // place mat for igniter relay
+const int led_green = 10;
+const int led_red = 11;
+
+/* Assign states to input commands */
+typedef enum State {
+        STOP = '1',
+        IGNITE = '2',
+        OPEN = '3',
+        WAIT = '4',
+        CLOSE = '5',
+        PING = '6'
+};
+
+State state;
+State temp;
+/* Function Prototypes */
+State stateMachine(State state);
+State serialDelay(int numofdelays, int delay_length, State next_state);
+State valveOpen();
+State valveClose();
+
+void setup()
+{
+        pinMode(relay, OUTPUT);
+        pinMode(led_green, OUTPUT);
+        pinMode(led_red, OUTPUT);
+
+        digitalWrite(relay, LOW);
+        digitalWrite(led_green, LOW);
+        digitalWrite(led_red, LOW);
+
+        Serial.begin(9600);
+        servo.attach(servoPin);
+        // initial state is close
+        state = (State)CLOSE;
+}
+
+void loop()
+{
+        /* Read input Commands */
+        if (Serial.available()) {
+                temp = (State)Serial.read();
+                Serial.println(temp);
+                state = temp;
+                // TO DO: some check statement if serial input is not a defined enum value
+        }
+        state = stateMachine(state);
+}
+
+/*State machine function */
+State stateMachine(State new_state)
+{
+        switch (new_state) {
+        case STOP:
+                servo.attach(servoPin);
+                digitalWrite(relay, LOW);
+                digitalWrite(led_green, HIGH);
+                digitalWrite(led_red, LOW);
+
+                servo.write(close_servo);
+                servo.detach();
+                //Serial.println("END");
+                new_state = (State)WAIT;
+                break;
+        case IGNITE:
+                digitalWrite(relay, HIGH);  
+                digitalWrite(led_green, LOW);
+                digitalWrite(led_red, HIGH);                           
+                new_state = serialDelay(20,100,(State)OPEN);
+                break;
+        case OPEN:
+                Serial.println("opening");
+                digitalWrite(relay, HIGH);
+                digitalWrite(led_green, HIGH);
+                digitalWrite(led_red, HIGH); 
+                new_state = valveOpen();
+                Serial.println("done");
+                break;
+        case CLOSE:
+                Serial.println("closing");
+                digitalWrite(relay, LOW);
+                digitalWrite(led_green, HIGH);
+                digitalWrite(led_red, LOW); 
+                new_state = valveClose();
+                new_state = (State)WAIT;
+                break;
+        case WAIT:
+                // delay until some command is given
+                digitalWrite(relay, LOW);
+                digitalWrite(led_green, HIGH);
+                digitalWrite(led_red, LOW); 
+                break;              
+        case PING:
+              Serial.println("I am ALIVE!");
+              new_state = (State)WAIT;
+              break;
+        default:
+                break;
+        }
+        return new_state;
+}
+
+/* Valve Open Sequence */
+State valveOpen(){
+        State state = OPEN;
+        servo.attach(servoPin);
+        for (int pos = 70; pos >= 0; pos -= 1) { // goes from 0 degrees to 180 degrees
+            // in steps of 1 degree
+            servo.write(pos);              // tell servo to go to position in variable 'pos'
+            serialDelay(2,5,OPEN);
+        }
+        state = serialDelay(20, 100, WAIT); //wait 2 seconds
+        servo.write(180); //open all the way
+          for (int pos = 100; pos <= 180; pos += 1) { // goes from 0 degrees to 180 degrees
+            // in steps of 1 degree
+            servo.write(pos);              // tell servo to go to position in variable 'pos'
+            serialDelay(2,5,OPEN);
+          }
+        state = serialDelay(100,100, CLOSE); //wait
+        return state;
+}
+
+/* Valve Closed */
+State valveClose(){
+        State state = CLOSE;
+        servo.attach(servoPin); //mount servo
+         for (int pos = 70; pos >= 0; pos -= 1) { // goes from 0 degrees to 180 degrees
+            // in steps of 1 degree
+            servo.write(pos);              // tell servo to go to position in variable 'pos'
+            serialDelay(2,5,state);
+        }
+        state = serialDelay(5, 10, state);
+        servo.detach();
+        return state;
+}
+
+/* delay Call back function */
+State serialDelay(int numofdelays, int delay_length, State next_state){
+        Serial.println(state);
+        State state = next_state;
+        for(int i=0; i<numofdelays; i++) {
+                delay(delay_length);
+                if (Serial.available()) {
+                        State rec_state = (State)Serial.read();
+                        if(rec_state == STOP) {
+                                Serial.println("STOPPED");
+                                state = STOP;
+                                break;
+                        }
+                }
+        }
+        return state;
+}
--- README.md
+++ README.md
@@ -1,9 +1,11 @@
 # UIgnite
 
- A basic UI made with Tkinter in python 2.7 for controlling engine ignition tests via XBee radios.  
- Built to work with Arduino-Iginiton.
+ A basic UI client made with Tkinter in python 2.7 for controlling engine ignition tests via XBee radios.  
+ Built to work with the Arduino server.
 ![screen shot 2017-10-25 at 10 24 07 am](https://user-images.githubusercontent.com/20449016/32208520-664bc2a6-bdd9-11e7-882c-718dc20fb6dc.png)
-## Running
+
+## Python Client
+### Running
 To run, make sure your system satisfies requirements.txt and that you are running python 2.7
 
 If you're not running python 2.7 and/or don't want to run python 2.7, you could always change `Tkinter` to `tkinter` in the import statement of `application.py` and it would probably work.
@@ -21,7 +23,7 @@ This would result in the serial port trying to open at `\dev\ttyUSB7`
 This would result in the serial port trying to open at
 `COM5`
 
-## Parameters
+### Parameters
 
 `platform` refers to the operating system you are running on.
 
@@ -37,3 +39,60 @@ uw|bash on windows (build 16299 and later)
 
 
 If you get permission issues on unix, try running it as superuser.
+
+## Arduino Server
+Arduino code for wireless hybrid engine ignition test rig.
+
+The igniter software is a state machine that is controlled by input from the serial port.  
+In our case, the serial port is used by an XBEE pro 900HP radio, giving us wireless control of the igniter.
+
+### State Machine
+
+The state machine has 6 states it can be in, each corresponding to a number:
+
+State|Number|Green LED|Red LED|Action
+:---:|:---:|:---:|:---:|---
+`STOP`|1|ON|OFF|Close the valve, turn the igniter relay off.
+`IGNITE`|2|OFF|ON|Turn the igniter relay on, starting the igniter.
+`OPEN`|3|ON|ON|Keep the igniter relay on, open the valve, keep it open for 10 seconds
+`CLOSE`|4|ON|OFF|Close the valve
+`WAIT`|5|ON|OFF|Do nothing
+`PING`|6|---|---|Send ``"I AM ALIVE!"`` to the base station over the radio
+
+The state machine is written such that the user can go to  the `STOP` state from any of the other states at any time. This is a safety feature, so that the engine test can be aborted and stopped at any time.
+
+### State Transitions
+The igniter starts in the `CLOSE` state.  It waits to receive serial input.
+
+The following are two typical state flows:
+###### Complete Run
+1. Power on -> `CLOSE`
+2. User presses `ping` button at base station -> `PING`
+3. User presses `start` button at base station -> `IGNITE`
+4. Automatic transition: `IGNITE`->`OPEN`
+5. Valve opens and stays open for 10 seconds
+6. Automatic transition: `OPEN`->`CLOSE`
+7. Automatic transition: `CLOSE`->`WAIT`
+
+###### Aborted Run
+1. Power on -> `CLOSE`
+2. User presses `ping` button at base station -> `PING`
+3. User presses `start` button at base station -> `IGNITE`
+4. Automatic transition: `IGNITE`->`OPEN`
+5. **User presses `stop` button at any time during any state -> `STOP`**
+6. Automatic transition: `STOP`->`CLOSE`
+7. Automatic transition: `CLOSE`->`WAIT`
+
+
+### Radio information:
+An Xbee Pro 900HP radio is used both for the igniter and the base station.
+
+The XBee packet includes a checksum, which ensures that the data received by one XBee is the same as the data sent by the other XBee.
+
+Parameter|Value|
+---|---  
+Specified Outdoor Range|	10 Kbps: up to 9 miles (14 km) ( w/ 2.1 dB dipole antennas)
+Tested Range | 100m
+Range to be used | 50m
+Transmit Power|	Up to 24 dBm (250 mW)  
+Receiver Sensitivity	| -110 dBm @ 10 Kbps
